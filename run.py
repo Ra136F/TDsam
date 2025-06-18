3081266ea915e401d1f1e932e29577cd2069dcf7
lxf <929055273@qq.com>
2025-06-18 12:14:16 +0800

    1

--- run.py
+++ run.py
@@ -8,10 +8,10 @@ from xender2cloud import xender_send
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='客户端传输')
-    parser.add_argument('-method', type=str, default='s', help="传输方式")
+    parser.add_argument('-method', type=str, default='x', help="传输方式")
     parser.add_argument('-data_name', type=str, default='oil-well', help="数据集名称")
     parser.add_argument('-target', type=str, default='T-JUS-CKP', help="目标特征")
-    parser.add_argument('-lambda_value', type=int, default=0.001, help="采样率")
+    parser.add_argument('-lambda_value', type=float, default=0.01, help="采样率")
     parser.add_argument('-mode', type=int, default=0, help="[0,1],不适用GPU、使用GPU")
     parser.add_argument('-ip', type=str, default='10.12.54.122', help="IP地址")
     parser.add_argument('-port', type=str, default='5002', help="端口")
