4d168edf68b329351ca3481f453b5ef3b33d61fb
lxf <929055273@qq.com>
2025-06-27 09:18:13 +0800

    a

--- run.py
+++ run.py
@@ -9,9 +9,9 @@ from xender2cloud import xender_send, fenlei_send
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='客户端传输')
-    parser.add_argument('-method', type=str, default='t', help="传输方式")
-    parser.add_argument('-data_name', type=str, default='oil-well-1', help="数据集名称")
-    parser.add_argument('-target', type=str, default='T-JUS-CKP', help="目标特征")
+    parser.add_argument('-method', type=str, default='a', help="传输方式")
+    parser.add_argument('-data_name', type=str, default='power-1', help="数据集名称")
+    parser.add_argument('-target', type=str, default='power_1s', help="目标特征")
     parser.add_argument('-lambda_value', type=float, default=0.1, help="采样率")
     parser.add_argument('-mode', type=int, default=0, help="[0,1],不适用GPU、使用GPU")
     parser.add_argument('-ip', type=str, default='10.12.54.122', help="IP地址")
