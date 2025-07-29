569bccc2dadcd419297216f2b91abf6f739ca967
lxf <929055273@qq.com>
2025-07-29 16:03:03 +0800

    zimpate

--- run.py
+++ run.py
@@ -10,9 +10,9 @@ from xender2cloud import xender_send, fenlei_send, fenlei_send2, fenlei_send2_yi
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='客户端传输')
     parser.add_argument('-method', type=str, default='c', help="传输方式")
-    parser.add_argument('-data_name', type=str, default='energy', help="数据集名称")
-    parser.add_argument('-target', type=str, default='T1', help="目标特征")
-    parser.add_argument('-lambda_value', type=float, default=0.25, help="采样率")
+    parser.add_argument('-data_name', type=str, default='household', help="数据集名称")
+    parser.add_argument('-target', type=str, default='Voltage', help="目标特征")
+    parser.add_argument('-lambda_value', type=float, default=1, help="采样率")
     parser.add_argument('-mode', type=int, default=0, help="[0,1],不适用GPU、使用GPU")
     parser.add_argument('-ip', type=str, default='10.12.54.122', help="IP地址")
     parser.add_argument('-port', type=str, default='5002', help="端口")
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     elif args.method == 'all' or args.method == 'a':
         all_send(args)
     elif args.method == 'guding' or args.method == 'g':
-        fenlei_send(args)
+        fenlei_send3(args)
     elif args.method == 'c' or args.method == 'cusum':
         fenlei_send2_yibu(args)
     else:
